4949df7757a2d1a7b2e8dbf6e9bdbade6cb4740d
John Weber <88911723+wbrJohn@users.noreply.github.com>
2021-10-22 03:52:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const LinksSocialMedia = {
+  github: 'wbrJohn',
+  facebook: 'jonathan.soaresweber',
+  instagram: 'wbrjohn'
+}
+
+function changeSocialMediaLinks() {
+  //document.getElementById('userName').textContent = 'Olivia'
+  //userName.textContent = 'Olivia'
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImg.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
+
+//Arrow functions ( => )
+/*function nomedafuncao(argumentos, argumento2){
+  //code
+}
+
+(argumento, argumento2) => {
+
+}
+
+() => {
+
+}
+*/
